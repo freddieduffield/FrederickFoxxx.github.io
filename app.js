55b0df9a12aaa284249d737cd20b1081c71b1a31
FrederickFoxxx <fwhduffield@gmail.com>
2017-09-22 16:44:36 +0100

    made frame and jQuery show hide

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-
 $(() => {
-  $('#show').click(function(){
-    $('#caro').toggle();
+  $('#work-link').click(function() {
+    $('div.caro').show('slow');
+    $('div.about-content').hide('slow');
+    $('div.contact-content').hide('slow');
   });
+
+  $('#about-link').click(function() {
+    $('div.about-content').show('slow');
+    $('div.caro').hide('slow');
+    $('div.contact-content').hide('slow');
+  });
+
+  $('#contact-link').click(function() {
+    $('div.contact-content').show('slow');
+    $('div.caro').hide('slow');
+    $('div.about-content').hide('slow');
+  });
+
+
 });
