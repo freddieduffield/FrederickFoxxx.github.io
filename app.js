46b63a5aca9097a1c5b25fabe603432ea9d5eead
FrederickFoxxx <fwhduffield@gmail.com>
2017-10-06 17:42:06 +0100

    tried to make it fit screen

--- app.js
+++ app.js
@@ -1,49 +1,51 @@
 $(() => {
+  $('#body').css('min-height', screen.height);
+
   $('#work-link').click(function() {
-    $('div.caro').show('slow');
+    $('div.work-cont').show('slow');
     $('div.about-content').hide('slow');
     $('div.contact-content').hide('slow');
   });
 
   $('#about-link').click(function() {
     $('div.about-content').show('slow');
-    $('div.caro').hide('slow');
+    $('div.work-cont').hide('slow');
     $('div.contact-content').hide('slow');
   });
 
   $('#contact-link').click(function() {
     $('div.contact-content').show('slow');
-    $('div.caro').hide('slow');
+    $('div.work-cont').hide('slow');
     $('div.about-content').hide('slow');
   });
 
-  // For the hover effects
-  if (Modernizr.touch) {
-    // show the close overlay button
-    $('.close-overlay').removeClass('hidden');
-    // handle the adding of hover class when clicked
-    $('.img').click(function(e){
-      if (!$(this).hasClass('hover')) {
-        $(this).addClass('hover');
-      }
-    });
-    // handle the closing of the overlay
-    $('.close-overlay').click(function(e){
-      e.preventDefault();
-      e.stopPropagation();
-      if ($(this).closest('.img').hasClass('hover')) {
-        $(this).closest('.img').removeClass('hover');
-      }
-    });
-  } else {
-    // handle the mouseenter functionality
-    $('.img').mouseenter(function(){
-      $(this).addClass('hover');
-    })
-    // handle the mouseleave functionality
-    .mouseleave(function(){
-      $(this).removeClass('hover');
-    });
-  }
+  // // For the hover effects
+  // if (Modernizr.touch) {
+  //   // show the close overlay button
+  //   $('.close-overlay').removeClass('hidden');
+  //   // handle the adding of hover class when clicked
+  //   $('.img').click(function(e){
+  //     if (!$(this).hasClass('hover')) {
+  //       $(this).addClass('hover');
+  //     }
+  //   });
+  //   // handle the closing of the overlay
+  //   $('.close-overlay').click(function(e){
+  //     e.preventDefault();
+  //     e.stopPropagation();
+  //     if ($(this).closest('.img').hasClass('hover')) {
+  //       $(this).closest('.img').removeClass('hover');
+  //     }
+  //   });
+  // } else {
+  //   // handle the mouseenter functionality
+  //   $('.img').mouseenter(function(){
+  //     $(this).addClass('hover');
+  //   })
+  //   // handle the mouseleave functionality
+  //   .mouseleave(function(){
+  //     $(this).removeClass('hover');
+  //   });
+  // }
 
 });
