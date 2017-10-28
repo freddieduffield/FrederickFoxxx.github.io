3d36ab85f2bd4bffcc1c1d919dcb757133c04b1b
FrederickFoxxx <fwhduffield@gmail.com>
2017-10-28 20:59:16 +0100

    refix

--- app.js
+++ app.js
@@ -5,47 +5,21 @@ $(() => {
     $('div.work-cont').show('slow');
     $('div.about-content').hide('slow');
     $('div.contact-content').hide('slow');
+    $('.header-ani').css('color', '#CBC5EA');
+
   });
 
   $('#about-link').click(function() {
     $('div.about-content').show('slow');
     $('div.work-cont').hide('slow');
     $('div.contact-content').hide('slow');
+    $('.header-ani').css('color', '#D46A6A');
   });
 
   $('#contact-link').click(function() {
     $('div.contact-content').show('slow');
     $('div.work-cont').hide('slow');
     $('div.about-content').hide('slow');
+    $('.header-ani').css('color', 'aquamarine');
   });
-
-  // // For the hover effects
-  // if (Modernizr.touch) {
-  //   // show the close overlay button
-  //   $('.close-overlay').removeClass('hidden');
-  //   // handle the adding of hover class when clicked
-  //   $('.img').click(function(e){
-  //     if (!$(this).hasClass('hover')) {
-  //       $(this).addClass('hover');
-  //     }
-  //   });
-  //   // handle the closing of the overlay
-  //   $('.close-overlay').click(function(e){
-  //     e.preventDefault();
-  //     e.stopPropagation();
-  //     if ($(this).closest('.img').hasClass('hover')) {
-  //       $(this).closest('.img').removeClass('hover');
-  //     }
-  //   });
-  // } else {
-  //   // handle the mouseenter functionality
-  //   $('.img').mouseenter(function(){
-  //     $(this).addClass('hover');
-  //   })
-  //   // handle the mouseleave functionality
-  //   .mouseleave(function(){
-  //     $(this).removeClass('hover');
-  //   });
-  // }
-
 });
